db56ef35e561b8b474abdaaf419c700bde9bffbd
Tribhuwan <mathincosmos@gmail.com>
2022-09-18 22:26:21 +0530

    Configure webpack with tailwindcss

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+        assetModuleFilename: '[name][ext]'
+
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            title: 'My Awesome application',
+            myPageHeader: 'Hello World',
+            template: 'src/template.html',
+            filename: 'index.html' //relative to root of the application
+        })
+    ]
+
+}
