43c5b3b6ef15629bfd2a05d3c026daf0dae716b5
Tribhuwan <mathincosmos@gmail.com>
2022-09-20 23:33:27 +0530

    add feature to toggle bg music

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
 import "./style.css";
+
+import icon from "./Images/icon.png";
+const iconTag = document.querySelector("#web-icon");
+iconTag.setAttribute("href", icon);
+
+import bgMusic from "./bgMusic.mp3";
+const audio = document.querySelector("audio");
+audio.volume = 0.6;
+const audioSrc = document.querySelector(".bgMusic");
+audioSrc.setAttribute("src", bgMusic);
+
+// add jukebox for music
+const jukebox = document.querySelector(".jukebox");
+jukebox.addEventListener("click", toggleImg);
+jukebox.addEventListener("click", toggleMusic);
+
+let isOn = false;
+function toggleMusic() {
+  if (isOn) {
+    audio.pause();
+    isOn = false;
+  } else {
+    audio.play();
+    isOn = true;
+  }
+}
+function toggleImg() {
+  if (isOn) {
+    jukebox.classList.add("bg-musicOff");
+    jukebox.classList.remove("bg-musicOn");
+  } else {
+    jukebox.classList.add("bg-musicOn");
+    jukebox.classList.remove("bg-musicOff");
+  }
+}
+
+async function getWeather(query) {
+  try {
+    const res = await fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?q=${query}&APPID=d48a46383954cbdee3198804107fd92d`,
+      {
+        mode: "cors",
+      }
+    );
+    if (res.status != 200) throw new Error("City Data unavailable");
+    else {
+      const data = await res.json();
+      console.log(data);
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+const city = "sweden";
+getWeather(city);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  content: ["./*/*.html", "./*/*.js"],
+  content: ["./*/*.html", "./src/index.js"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        musicOn: "url('./Images/musicOn.png')",
+        musicOff: "url('./Images/musicOff.png')",
+        bgImage: "url('./Images/bgImage.jpg')",
+      },
+    },
   },
   variants: {
     extend: {},
